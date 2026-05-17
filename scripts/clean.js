e4cac2c6548294346b5fb283e004df6226dfb810
Rémi Thebault <remi.thebault@gmail.com>
2026-05-17 21:48:26 +0200

    avoid  expired cache

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,11 +1,15 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-const distDir = path.resolve(__dirname, '../dist');
-const distDevDir = path.resolve(__dirname, '../dist-dev');
-const wasmDir = path.resolve(__dirname, '../src/wasm');
+const DIRS_TO_CLEAN = [
+    path.resolve(__dirname, '../dist'),
+    path.resolve(__dirname, '../dist-dev'),
+    path.resolve(__dirname, '../src/wasm'),
+    path.resolve(__dirname, '../examples/dist'),
+    path.resolve(__dirname, '../examples/node_modules/.vite'),
+];
 
-for (const dir of [distDir, distDevDir, wasmDir]) {
+for (const dir of DIRS_TO_CLEAN) {
     if (fs.existsSync(dir)) {
         console.log(`Removing ${dir}...`);
         fs.rmSync(dir, { recursive: true, force: true });
