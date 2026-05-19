ee4c9f9f9da2110c51dbada8a11bbeca6b51fe25
Rémi Thebault <remi.thebault@gmail.com>
2026-05-19 21:28:26 +0200

    examples: styling a bit

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -3,13 +3,33 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
+import { definePreset } from '@primeuix/themes'
+import { content } from '@primeuix/themes/aura/accordion'
 
 const app = createApp(App)
 
 app.use(createPinia())
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        colorScheme: {
+            light: {
+                content: {
+                    background: '{surface.100}',
+                }
+            },
+            dark: {
+                content: {
+                    background: '{surface.800}',
+                }
+            }
+        }
+    }
+})
+
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
+        preset: MyPreset,
         options: {
             darkModeSelector: '.app-dark-mode',
         }
