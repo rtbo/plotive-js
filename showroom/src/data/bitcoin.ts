1edab85902b3c879b08c163d4615adfbf83d27b9
Rémi Thebault <remi.thebault@gmail.com>
2026-08-02 13:58:15 +0200

    mv showroom in its own repo

--- showroom/src/data/bitcoin.ts
+++
@@ -1,7 +0,0 @@
-import BcCsv from "@/data/BTC-USD.csv?raw";
-import { parseCsv } from "plotive";
-import type { DataCol } from "plotive";
-
-export async function getBcData(): Promise<Record<string, DataCol>> {
-    return parseCsv(BcCsv);
-}
